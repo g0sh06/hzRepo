072ab9fd30259d96d3e3dd74c391a573b5476806
g0sh06 <s.atanasov51@gmail.com>
2023-09-22 16:16:28 +0200

    Message

--- starter-code/assets/js/app.js
+++ starter-code/assets/js/app.js
@@ -1,5 +1,8 @@
 // Prove that JavaScript is working in your browser. You may delete this.
 name = window.prompt()
 duration = window.prompt()
-console.log('JavaScript is working!');
+console.log('Hello' + name + ', Your input was' + duration);
+hours = Math.floor(duration / 60)
+minutes = duration % 60;
+console.log('Output:' + rest + 'hours and' + minutes + 'minutes')
 
