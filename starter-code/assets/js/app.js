1684d4fd3341a921a12a442a28d78d5ec1d0cb14
g0sh06 <s.atanasov51@gmail.com>
2023-09-25 17:06:35 +0200

    First feedback changes

--- starter-code/assets/js/app.js
+++ starter-code/assets/js/app.js
@@ -1,8 +1,8 @@
 // Prove that JavaScript is working in your browser. You may delete this.
 const name = window.prompt('Gimme your name');
 const duration = window.prompt('Give the duration in minutes you want to convert');
-document.write('Hello ' + name + ', Your input was ' + duration + ' minutes\n');
+document.write('Hello\n' + name + ',\nYour input was\n' + duration + '\nminutes\n');
 const hours = ~~(duration / 60);
 const minutes = duration % 60;
-document.write('Output: ' + hours + ' hours and ' + minutes + ' minutes');
+document.write('Output:\n' + hours + '\nhours and\n' + minutes + '\nminutes');
 
